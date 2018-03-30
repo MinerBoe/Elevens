2e0c332f696cb1f55bf50f6d2ee5cc29aa34eb67
Mats Dale <14minerboe16@gmail.com>
2018-03-30 10:52:27 -0600

    Activity 1

--- src/CardTester.java
+++ src/CardTester.java
@@ -8,6 +8,26 @@ public class CardTester {
 	 *	@param args is not used.
 	 */
 	public static void main(String[] args) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+		Card queenHearts = new Card("Queen", "Hearts", 12);
+		Card kingSpades = new Card("King", "Spades", 13);
+		Card twoDiamonds = new Card("Two", "Diamonds", 2);
+
+		System.out.println(queenHearts.suit());
+		System.out.println(queenHearts.rank());
+		System.out.println(queenHearts.pointValue());
+		System.out.println(queenHearts.matches(kingSpades));
+		System.out.println(queenHearts.toString());
+
+		System.out.println(kingSpades.suit());
+		System.out.println(kingSpades.rank());
+		System.out.println(kingSpades.pointValue());
+		System.out.println(kingSpades.matches(queenHearts));
+		System.out.println(kingSpades.toString());
+
+		System.out.println(twoDiamonds.suit());
+		System.out.println(twoDiamonds.rank());
+		System.out.println(twoDiamonds.pointValue());
+		System.out.println(twoDiamonds.matches(queenHearts));
+		System.out.println(twoDiamonds.toString());
 	}
 }
